44f9eee78eab8615b29321601c0d2f1688d28de0
rAhUl7500791 <rahulbisht1009@gmail.com>
2025-08-22 18:08:59 +0530

    'changes'

--- appbackend/backend/settings.py
+++ appbackend/backend/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 STATIC_URL = '/static/'
 
 # deployment ke liye extra
